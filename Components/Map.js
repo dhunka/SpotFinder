e1ca4461611167218b26bd29c8b8e0999dca7b91
dhunka <matiasguzman45@gmail.com>
2023-05-18 01:13:28 -0400

    se agrega el mapa

--- Components/Map.js
+++ Components/Map.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
-import MapView from "react-native-maps";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps'
+import tw from "tailwind-react-native-classnames";
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
-import { Text,StyleSheet,View } from "react-native";
 
-export default class Map extends Component{
-   
-    render(){
-        return(
-            <View style={styles.container}> 
-                <MapView
-               style={styles.map}
-                />
-            </View>
-        )
-    }
+const Map = () => {
+    const origin = useSelector(selectOrigin);
+  return (
+    <View style={tw`flex-1`}>
+      <MapView
+      style={tw`flex-1`}
+      initialRegion={{
+        latitude:origin.location.lat,
+        longitude:origin.location.lng,
+        latitudeDelta:0.005,
+        longitudeDelta:0.005,
+      }}
+      />
+    </View>
+  )
 }
 
-const styles= StyleSheet.create({
-    container:{
-        flex:1,
-    },
-    map:{
-        flex:1,
-    }
-})
+export default Map
+
+const styles = StyleSheet.create({})
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Text,StyleSheet,View } from "react-native";
 import Map from "../Components/Map";
-
+import MapView from 'react-native-maps'
+import tw from "tailwind-react-native-classnames";
 const MapScreen = () =>{
     return(
         <View>
+            <View style={tw`h-2/3`}>
              <Map/>
+            </View>
+            <View style={tw`h-1/2`}>
+                
+            </View>
         </View>
     )
 }
