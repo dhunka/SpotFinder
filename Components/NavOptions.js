784e24e6d3782a76b1e008b4dc938384a851dad4
diegom0n <die.fuentes@duocuc.cl>
2023-06-11 22:16:32 -0400

    botones en vertical

--- Components/NavOptions.js
+++ Components/NavOptions.js
@@ -1,65 +1,64 @@
-import { StyleSheet, Text, View,FlatList,TouchableOpacity,Image} from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import tw from "tailwind-react-native-classnames";
 import { Icon } from 'react-native-elements';
 import { useNavigation } from 'expo-router';
 
-const data =[
-    {
-        id:"123",
-        tittle:"Buscar Estacionamiento",
-        image:"https://links.papareact.com/3pn",
-        screen:"MapScreen",
-    },
-    {
-        id:"1234",
-        tittle:"Mis Estacionamientos",
-        image:"https://links.papareact.com/3pn",
-        screen:"MapScreen",
-    },
-    {
-        id:"12345",
-        tittle:"Iniciar Sesión",
-        image:"https://links.papareact.com/3pn",
-        screen:"LoginScreen",
-    },
-    {
-        id:"123456",
-        tittle:"Registrarse",
-        image:"https://links.papareact.com/3pn",
-        screen:"RegisterScreen",
-    },
-    {
-      id:"1234567",
-      tittle:"Agregar Vehículo",
-      image:"https://links.papareact.com/3pn",
-      screen:"NewCarScreen",
+const data = [
+  {
+    id: "123",
+    title: "Buscar Estacionamiento",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "1234",
+    title: "Mis Estacionamientos",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "12345",
+    title: "Iniciar Sesión",
+    image: "https://links.papareact.com/3pn",
+    screen: "LoginScreen",
+  },
+  {
+    id: "123456",
+    title: "Registrarse",
+    image: "https://links.papareact.com/3pn",
+    screen: "RegisterScreen",
+  },
+  {
+    id: "1234567",
+    title: "Agregar Vehículo",
+    image: "https://links.papareact.com/3pn",
+    screen: "NewCarScreen",
   },
 ]
 
 const NavOptions = () => {
+  const navigation = useNavigation();
 
-    const navigation= useNavigation();
   return (
     <View>
       <FlatList
-       data={data}
-       horizontal
-       keyExtractor={(item)=>item.id}
-       renderItem={({item}) =>(
-        <TouchableOpacity 
-          onPress={()=>navigation.navigate(item.screen)}
-          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 flex-1`}
+          >
             <View>
-               <Image
-                 style={{width:120,height:120, resizeMode:"contain"}}
-                 source={{uri: item.image}}
-               />
-               <Text style={tw`mt-2 text-sm font-semibold`}>{item.tittle}</Text>
-               
+              <Image
+                style={{ width: 120, height: 120, resizeMode: "contain" }}
+                source={{ uri: item.image }}
+              />
+              <Text style={tw`mt-2 text-sm font-semibold`}>{item.title}</Text>
             </View>
-         </TouchableOpacity>
-       )}
+          </TouchableOpacity>
+        )}
       />
     </View>
   )
