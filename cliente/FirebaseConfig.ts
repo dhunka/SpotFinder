6ad4236d5569f1862f533db0b4a351d9de720e3a
daigopez <daigofez@gmail.com>
2023-06-20 00:21:47 -0400

    se agrega recuperar contraseña

--- cliente/FirebaseConfig.ts
+++ cliente/FirebaseConfig.ts
@@ -18,4 +18,3 @@ export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
